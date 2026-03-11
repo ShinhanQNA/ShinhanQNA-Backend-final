5f6394fb992adfc814888db24244710038bb749b
bindoong01 <mailsky9687@gmail.com>
2026-03-11 17:33:08 +0900

    [Feat] PostLike 게시글 좋아요 엔티티 추가 (#13)

---
+++ src/main/java/back/sw/domain/like/entity/PostLike.java
@@ -0,0 +1,51 @@
+package back.sw.domain.like.entity;
+
+import back.sw.domain.member.entity.Member;
+import back.sw.domain.post.entity.Post;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(
+        name = "post_likes",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "uq_post_likes_post_user", columnNames = {"post_id", "user_id"})
+        }
+)
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PostLike {
+    @EmbeddedId
+    private PostLikeId id;
+
+    @Getter(AccessLevel.NONE)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @MapsId("postId")
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    @Getter(AccessLevel.NONE)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @MapsId("userId")
+    @JoinColumn(name = "user_id", nullable = false)
+    private Member member;
+
+    private PostLike(Post post, Member member) {
+        this.id = PostLikeId.of(post.getId(), member.getId());
+        this.post = post;
+        this.member = member;
+    }
+
+    public static PostLike create(Post post, Member member) {
+        return new PostLike(post, member);
+    }
+}

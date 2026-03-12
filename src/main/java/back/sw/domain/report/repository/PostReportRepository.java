8d4a82e07f137c00cb90f39a2d4f8187093ccf6e
bindoong01 <mailsky9687@gmail.com>
2026-03-12 09:25:29 +0900

    [Feat] PostReportRepository 관리자 신고 목록 조회 메서드 추가 (#19)

--- src/main/java/back/sw/domain/report/repository/PostReportRepository.java
+++ src/main/java/back/sw/domain/report/repository/PostReportRepository.java
@@ -3,6 +3,10 @@ package back.sw.domain.report.repository;
 import back.sw.domain.report.entity.PostReport;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostReportRepository extends JpaRepository<PostReport, Integer> {
     boolean existsByPostIdAndMemberId(int postId, int memberId);
+
+    List<PostReport> findAllByOrderByCreateDateDescIdDesc();
 }

b75a7d55fa20911baf738c0f6c48b3bc5e129c67
bindoong01 <mailsky9687@gmail.com>
2026-03-12 11:23:07 +0900

    [Refactor] MemberController 인증 주입 전환과 잘못된 토큰 401 테스트 추가 (#25)

--- src/main/java/back/sw/domain/member/controller/MemberController.java
+++ src/main/java/back/sw/domain/member/controller/MemberController.java
@@ -1,19 +1,19 @@
 package back.sw.domain.member.controller;
 
-import back.sw.domain.auth.service.AuthService;
 import back.sw.domain.member.dto.request.MemberJoinRequest;
 import back.sw.domain.member.dto.request.NicknameUpdateRequest;
 import back.sw.domain.member.dto.response.MemberJoinResponse;
 import back.sw.domain.member.dto.response.NicknameResponse;
 import back.sw.domain.member.service.MemberService;
 import back.sw.global.response.RsData;
+import back.sw.global.security.AuthenticatedMember;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,7 +22,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class MemberController {
     private final MemberService memberService;
-    private final AuthService authService;
 
     @PostMapping
     public ResponseEntity<RsData<MemberJoinResponse>> join(@Valid @RequestBody MemberJoinRequest request) {
@@ -34,10 +33,10 @@ public class MemberController {
 
     @PatchMapping("/me/nickname")
     public RsData<NicknameResponse> changeNickname(
-            @RequestHeader("Authorization") String authorization,
+            @AuthenticationPrincipal AuthenticatedMember authenticatedMember,
             @Valid @RequestBody NicknameUpdateRequest request
     ) {
-        int memberId = authService.getMemberIdFromAuthorizationHeader(authorization);
+        int memberId = authenticatedMember.memberId();
         NicknameResponse data = memberService.changeNickname(memberId, request.nickname());
 
         return new RsData<>("200-1", "닉네임이 수정되었습니다.", data);

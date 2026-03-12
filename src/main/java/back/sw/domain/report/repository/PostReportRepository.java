f316c77044e7a3a4fa7e0e77adefb568db50ae65
bindoong01 <mailsky9687@gmail.com>
2026-03-12 09:54:05 +0900

    [Refactor] PostReportRepository 관리자 신고목록 페이징/엔티티그래프 적용 (#19)

--- src/main/java/back/sw/domain/report/repository/PostReportRepository.java
+++ src/main/java/back/sw/domain/report/repository/PostReportRepository.java
@@ -1,12 +1,15 @@
 package back.sw.domain.report.repository;
 
 import back.sw.domain.report.entity.PostReport;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
+import org.springframework.data.jpa.repository.EntityGraph;
 
 public interface PostReportRepository extends JpaRepository<PostReport, Integer> {
     boolean existsByPostIdAndMemberId(int postId, int memberId);
 
-    List<PostReport> findAllByOrderByCreateDateDescIdDesc();
+    @Override
+    @EntityGraph(attributePaths = {"post", "member"})
+    Page<PostReport> findAll(Pageable pageable);
 }

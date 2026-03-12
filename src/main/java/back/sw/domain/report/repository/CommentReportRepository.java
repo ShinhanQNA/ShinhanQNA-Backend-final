66b069613dbec3cf1fcdb60ac93a5cec01a93e15
bindoong01 <mailsky9687@gmail.com>
2026-03-12 09:25:40 +0900

    [Feat] CommentReportRepository 관리자 신고 목록 조회 메서드 추가 (#19)

--- src/main/java/back/sw/domain/report/repository/CommentReportRepository.java
+++ src/main/java/back/sw/domain/report/repository/CommentReportRepository.java
@@ -3,6 +3,10 @@ package back.sw.domain.report.repository;
 import back.sw.domain.report.entity.CommentReport;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentReportRepository extends JpaRepository<CommentReport, Integer> {
     boolean existsByCommentIdAndMemberId(int commentId, int memberId);
+
+    List<CommentReport> findAllByOrderByCreateDateDescIdDesc();
 }
